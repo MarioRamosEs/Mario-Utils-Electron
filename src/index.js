f2bb9abc944455329c24a86e423c8eac93c80a40
Mario Ramos Montesinos <mariodarken@gmail.com>
2020-08-30 00:03:15 +0200

    .

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const { app, Menu, Tray, Notification } = require("electron");
 const exec = require("child_process").exec;
 
 const isWin = process.platform === "win32";
+const mcuIp = "192.168.1.173";
 
 let tray = null;
 
+//No funciona 
 function notif(value){
   console.log(value);
   new Notification("Test", {
@@ -15,10 +17,6 @@ function notif(value){
 
 app.on("ready", () => {
   tray = new Tray(path.join(__dirname, "./../assets/icon.png"));
-
-  if (isWin) {
-    tray.on("click", tray.popUpContextMenu);
-  }
 
   const menu = Menu.buildFromTemplate([
     {
@@ -32,10 +30,22 @@ app.on("ready", () => {
       },
     },
     {
+      label: "Apagar 30 minutos",
+      click() {
+        if (isWin) {
+          exec("shutdown /s /t 1800");
+        } else {
+          notif("TODO");
+        }
+      },
+    },
+    {
       label: "Lolete",
       click() {
         if (isWin) {
-          notif("TODO en Windows");
+          exec("C:\\Users\\mario\\AppData\\Local\\Discord\\Update.exe --processStart Discord.exe");
+          exec("C:\\Users\\mario\\AppData\\Local\\Programs\\Blitz\\Blitz.exe");
+          exec("\"E:\\Games\\Riot Games\\Riot Client\\RiotClientServices.exe\" --launch-product=league_of_legends --launch-patchline=live");
         } else {
           exec("open -a Blitz");
           exec("open -a LeagueClient");
@@ -60,4 +70,4 @@ app.on("ready", () => {
   tray.setContextMenu(menu);
 });
 
-app.dock.hide();
+if(!isWin) app.dock.hide();
